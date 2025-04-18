9ce8640c5274cade83aff4c70bfcbc3c89a8c151
Giulia2424 <giuliarom4@gmail.com>
2025-04-18 18:33:43 +0200

    multigeo.m

--- multigeo.m
+++ multigeo.m
@@ -7,10 +7,10 @@ function [k]=multigeo(A,l,toll)
 %   k naturale positivo, dimensione dell’autospazio
 %Tale function deve chiamare la fattorizzazione LU di matlab e calcolare quanti elementi diagonali di U 
 % risultano essere in modulo sotto la soglia passata dall’utente
-[L,U] = lu(A);
+B = A-l*eye(size(A));
+[L,U] = lu(B);
 k=0;
-i=1;
-while i<=size(U,1)
+for i=1:size(U,1)
     if abs(U(i,i)) <toll
         k=k+1;
     end
